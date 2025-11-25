ed3f296f20f711c37efbc5c67e3e2c4ffd9a2c8d
PaulaReyero <paula.reyeromartinez@usp.ceu.es>
2025-11-25 13:26:33 +0100

    añadir doctor ID

--- src/main/java/main/DoctorMenu.java
+++ src/main/java/main/DoctorMenu.java
@@ -83,14 +83,14 @@ public class DoctorMenu {
     private void viewHistory() {
         viewPatients();
         int clientId = UIUtils.readInt("\nEnter the ID of the patient you want to see: ");
-        String result = service.getPatientHistory(conn, clientId);
+        String result = service.getPatientHistory(conn, doctor.getDoctorId(), clientId);
         System.out.println(result);
     }
 
     private void viewSignals() {
         viewPatients();
         int clientId = UIUtils.readInt("\nEnter the ID of the patient you want to see: ");
-        String result = service.getPatientSignals(conn, clientId);
+        String result = service.getPatientSignals(conn, doctor.getDoctorId(), clientId);
         System.out.println(result);
     }
 
@@ -98,7 +98,7 @@ public class DoctorMenu {
 
         viewPatients();
         int clientId = UIUtils.readInt("\nEnter patient ID to observe: ");
-        List<Integer> recordIds = service.getRecordIdsOfPatient(conn, clientId);
+        List<Integer> recordIds = service.getRecordIdsOfPatient(conn, doctor.getDoctorId(), clientId);
 
         if (recordIds.isEmpty()) {
             System.out.println("This patient has no medical history.");
